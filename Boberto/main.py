9dd6b06d9952e4136bc6ec1b8f9ea1273598e9ff
paperfisher <gabrielrochappf@gmail.com>
2024-11-29 15:56:26 -0300

    Update main.py

--- Boberto/main.py
+++ Boberto/main.py
@@ -8,11 +8,18 @@ from pybricks.ev3devices import UltrasonicSensor, ColorSensor
 from pybricks.parameters import Stop, Direction
 from pybricks.tools import wait
 
+
+import time
+
+
 # Initialize the EV3
 ev3 = EV3Brick()
 
-sensorLinha = LUMPDevice(Port.S4)
+sensorLinha = LUMPDevice(Port.S3)
 valoresSensorLinha = sensorLinha.read(2)
+
+
+giroscopio = LUMPDevice(Port.S2)
 
 # sensores do vendra
 # cor do sensor da esquerda do meio
@@ -31,59 +38,247 @@ def CorDireitaVendra():
 def CorDireitaEXvendra():
     return sensorLinha.read(2)[3]
 
-print(valoresSensorLinha)
-print(CorDireitaVendra())
+## direita e da esquerda juntos
+
+def sensoresDir():
+    return (CorDireitaVendra() + CorDireitaEXvendra()) / 2
+
+def sensoresEs():
+    return (CorEsquerdaEXvendra() + CorEsquerdaVendra()) / 2
 
 # o index 0 é o da direita
+
+def tudobranco():
+    if CorEsquerdaVendra() > branco and CorEsquerdaEXvendra() > branco and CorDireitaVendra() > branco and CorDireitaEXvendra() > branco:
+        return True
+    else:
+        return False
+
+##giroscopio
+
+def giro():
+    return giroscopio.read(0)[2]
+
 
 motor_d_esquerdo = Motor(Port.A, Direction.COUNTERCLOCKWISE) #horario
 motor_a_direito = Motor(Port.B, Direction.CLOCKWISE) #antihorario
 
-robot = DriveBase(motor_a_direito, motor_d_esquerdo, 50, 123)
+bobo = DriveBase(motor_a_direito, motor_d_esquerdo, 50, 123)
+
+branco = 68
+preto = 40
 
 
-#     motorDireito=motor_d,
-#     motorEsquerdo=motor_a,
-#     diametroRoda=50,
-#     distanciaEntreAsRodas=123
-# )
+kp = 1.2 #1
+kd = 1.8 # 0.5
 
-# # Initialize the motors.
-# left_motor = Motor(Port.B)
-# right_motor = Motor(Port.C)
 
-# # Initialize the color sensor.
-# line_sensor = ColorSensor(Port.S3)
+#os melhores valores foram 1.5 e 1.5
 
-# # Initialize the drive base.
-# robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+def erro():
+    erro = (CorEsquerdaEXvendra() + CorEsquerdaVendra()) - (CorDireitaVendra() + CorDireitaEXvendra())
+    return erro
 
-# Calculate the light threshold. Choose values based on your measurements.
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
+   
 
-# Set the drive speed at 100 millimeters per second.
-DRIVE_SPEED = 100
+def pd():
+    p = erro() * kp 
+    d = erro() - erro_anterior * kd
+    return p + d
 
-# Set the gain of the proportional line controller. This means that for every
-# percentage point of light deviating from the threshold, we set the turn
-# rate of the drivebase to 1.2 degrees per second.
 
-# For example, if the light value deviates from the threshold by 10, the robot
-# steers at 10*1.2 = 12 degrees per second.
-PROPORTIONAL_GAIN = 5
+def seguirlinha():
+    
+    vb = 60
+    valor = pd()
 
-# Start following the line endlessly.
+    motor_d_esquerdo.dc(vb + valor)
+    motor_a_direito.dc(vb - valor)
+
+    
+    erro_anterior = erro()
+
+    wait(25)
+
+
+def checar_90():
+    if sensoresEs() > branco and sensoresDir() < preto:
+        print("noventa dir")
+        
+        return "dir"
+    elif sensoresDir() > branco and sensoresEs() < preto:
+        print("noventa esq")
+        print(CorEsquerdaEXvendra(), CorEsquerdaVendra(), CorDireitaVendra(), CorDireitaEXvendra())
+        return "esq"
+    else:
+        False
+
+def pararMotores():
+    stop()
+
+def NoventaDir():
+    atual = giro()
+
+    while giro() < (atual + 90):
+        # print(giro())
+
+        motor_d_esquerdo.dc(50)
+        motor_a_direito.dc(-50)
+
+def NoventaEsq():
+    atual = giro()
+
+    while giro() > (atual - 90):
+        # print(giro())
+
+        motor_d_esquerdo.dc(-50)
+        motor_a_direito.dc(50)
+
+a = 1
+def e_gap():
+    global a
+    for i in range(a):
+        pararMotores()
+        bobo.straight(10)
+        a = a + 1
+        if a > 10:
+            bobo.straight(-a * 10)
+            return False
+            break
+        if tudobranco() == False:
+            pass
+        else:
+            return False
+
+def gap():
+    while tudobranco() == True:
+        pararMotores()
+        bobo.straight(10)
+
+
+def girargraus(graus, direcao):
+
+    if direcao == "esq":
+
+        atual = giro()
+
+        while giro() < (atual + graus):
+            # print(giro())
+
+            motor_d_esquerdo.dc(50)
+            motor_a_direito.dc(-50)
+
+    if direcao == "dir":
+
+        atual = giro()
+
+        while giro() > (atual - graus):
+            # print(giro())
+
+            motor_d_esquerdo.dc(-50)
+            motor_a_direito.dc(50)
+
+def gira_ate_achar():
+
+    if tudobranco() == True:
+
+        #verifica se é gap
+        if e_gap() == True:
+            gap()
+
+        else:
+            girargraus(30, "dir")
+            if tudobranco() == True:
+                girargraus(60, "esq")
+
+                if tudobranco() == True:
+
+                    while tudobranco() == True:
+
+                        girargraus(15, "esq")
+                    
+
+def noventa_semverde():
+
+    if checar_90() == "dir": 
+
+        motor_d_esquerdo.dc(50)
+        motor_a_direito.dc(50)
+
+        wait(300)
+
+        if tudobranco() == True:
+            
+            print(CorEsquerdaEXvendra(), CorEsquerdaVendra(), CorDireitaVendra(), CorDireitaEXvendra())
+            pararMotores()  
+            NoventaDir()
+
+            motor_d_esquerdo.dc(-50)
+            motor_a_direito.dc(-50)
+
+            wait(400)
+
+            if tudobranco() == True:
+
+                girargraus(30, "dir")
+                if tudobranco() == True:
+                    girargraus(60, "esq")
+
+                    if tudobranco() == True:
+
+                        while tudobranco() == True:
+
+                            girargraus(15, "esq")
+                    
+            
+    if checar_90() == "esq":
+
+        motor_d_esquerdo.dc(50)
+        motor_a_direito.dc(50)
+
+        wait(400)
+
+        if tudobranco() == True:
+
+            print(CorEsquerdaEXvendra(), CorEsquerdaVendra(), CorDireitaVendra(), CorDireitaEXvendra())
+            pararMotores()  
+            NoventaEsq()
+
+            motor_d_esquerdo.dc(-50)
+            motor_a_direito.dc(-50)
+
+            wait(300)
+
+            if tudobranco() == True:
+
+                girargraus(30, "dir")
+                if tudobranco() == True:
+                    girargraus(60, "esq")
+
+                    if tudobranco() == True:
+
+                        while tudobranco() == True:
+
+                            girargraus(15, "esq")
+                            
+
+erro_anterior = 0
 while True:
-    # Calculate the deviation from the threshold.
-    deviation = CorDireitaVendra() - threshold
+    if e_gap() == True:
+        gap()
 
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
+    noventa_semverde()
+    gira_ate_achar()
 
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
+    p = erro() * kp 
+    d = erro() - erro_anterior * kd
+    
+    vb = 40
+    valor = p + d
 
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
+    motor_d_esquerdo.dc(vb + valor)
+    motor_a_direito.dc(vb - valor)
+
+    erro_anterior = erro()
+
+    wait(25)
